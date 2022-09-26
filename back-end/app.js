17242e8937c740145dbb1173fd69bbfa67956206
Soupy710 <swaroopbhat710@gmail.com>
2022-09-26 09:03:12 +0530

    modified server to support lapca

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const app =express()
 const PORT=5000
+const axios = require('axios')
 
 app.use(express.json())
 app.get('/',(req,res)=>{
@@ -9,7 +10,7 @@ app.get('/',(req,res)=>{
 
 app.post('/',(req,res)=>{
     const {code,form,language}=req.body     
-    console.log(language)
+    axios.post('http://127.0.0.1:3003/getResults',{code,form,language}).then((result)=>{console.log(result['data'])}).catch((e)=>{console.log(e)})
     return res.json({code:code,form:form,language:language})
 })
 
