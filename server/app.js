2630ec555e6ed5d0872a239cd3a5579423b665ec
manish-22 <manishreddym22@gmail.com>
2022-09-30 12:39:38 +0530

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,23 +1,19 @@
 const express=require('express')
+var cors = require('cors')
 const app =express()
 const PORT=5000
 const axios = require('axios')
-
+app.use(cors())
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send("Hello world")
-})
 
 app.post('/',(req,res)=>{
     const {code,form,language}=req.body 
     console.log(code,form,language) 
     axios.post('http://127.0.0.1:3003/getResults',{code,form,language})
-    //.then((result)=>{console.log(typeof(result.data))})
     .then((ans)=>{
         console.log(ans.data)
         return res.json(ans.data)})
     .catch((e)=>{console.log(e)})
-   // return res.json({code:code,form:form,language:language})/
 })
 
 
