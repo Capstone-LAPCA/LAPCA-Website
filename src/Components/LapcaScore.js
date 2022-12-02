eba121e8918055bb28b32278180e0852fff562ff
manish-22 <manishreddym22@gmail.com>
2022-12-02 16:19:44 +0530

    Resolve dependancy

--- src/Components/LapcaScore.js
+++ src/Components/LapcaScore.js
@@ -1,9 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
 import { alpha, styled } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import '../Styles/LapcaScore.css';
